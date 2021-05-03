2fe9c5c7e856b470269d1c036dfd6164db28e0a4
EduJaucha <agredu1941@gmail.com>
2021-05-03 17:14:01 -0500

    postgre a postgresql

--- app/config.py
+++ app/config.py
@@ -15,7 +15,7 @@ class Config:
     PASS='admin'
     HOST='localhost'
     NAME_DB='TIENDA'
-    DATABASE_URL = 'postgres://fbzesginhgxytp:7c5c2ce2a868e7c8330d07188ae9a6ab8c126f43fe823859c85f55c63e6d13b3@ec2-3-217-219-146.compute-1.amazonaws.com:5432/d80so7bhl4ra3h'
+    DATABASE_URL = 'postgresql://fbzesginhgxytp:7c5c2ce2a868e7c8330d07188ae9a6ab8c126f43fe823859c85f55c63e6d13b3@ec2-3-217-219-146.compute-1.amazonaws.com:5432/d80so7bhl4ra3h'
 
     SECRET_KEY = 'llave_secreta'
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
