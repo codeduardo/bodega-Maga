233b4b0e34171d615b04a6010f1cd5bc7758604a
EduJaucha <agredu1941@gmail.com>
2021-05-03 14:52:32 -0500

    update database url

--- app/config.py
+++ app/config.py
@@ -1,14 +1,24 @@
 
 
+# class Config:
+#     USER='postgres'
+#     PASS='admin'
+#     HOST='localhost'
+#     NAME_DB='TIENDA'
+#     FULL_URL_DB = f'postgresql://{USER}:{PASS}@{HOST}/{NAME_DB}'
+
+#     SECRET_KEY = 'llave_secreta'
+#     SQLALCHEMY_DATABASE_URI = FULL_URL_DB
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
 class Config:
     USER='postgres'
     PASS='admin'
     HOST='localhost'
     NAME_DB='TIENDA'
-    FULL_URL_DB = f'postgresql://{USER}:{PASS}@{HOST}/{NAME_DB}'
+    DATABASE_URL = f'postgresql://{USER}:{PASS}@{HOST}/{NAME_DB}'
 
     SECRET_KEY = 'llave_secreta'
-    SQLALCHEMY_DATABASE_URI = FULL_URL_DB
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     
