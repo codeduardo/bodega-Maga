60e61133950a543a05337a7a02a0faf34c62ccb5
EduJaucha <agredu1941@gmail.com>
2021-05-03 15:29:01 -0500

    update database url 3

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-
+import os
 
 # class Config:
 #     USER='postgres'
@@ -18,7 +18,7 @@ class Config:
     DATABASE_URL = 'postgres://fbzesginhgxytp:7c5c2ce2a868e7c8330d07188ae9a6ab8c126f43fe823859c85f55c63e6d13b3@ec2-3-217-219-146.compute-1.amazonaws.com:5432/d80so7bhl4ra3h'
 
     SECRET_KEY = 'llave_secreta'
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_DATABASE_URI = os.environ.get(DATABASE_URL)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     
