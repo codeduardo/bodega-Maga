b03fc06d743f232e3c97eed10f4aa9f8c0d79762
EduJaucha <agredu1941@gmail.com>
2021-05-03 17:03:47 -0500

    working with context

--- create_db.py
+++ create_db.py
@@ -1,2 +1,9 @@
-from app import db
-db.create_all()
+import click
+from flask.cli import with_appcontext
+from app.migrate import init_db()
+
+
+@click.command(name='create_tables')
+@with_appcontext
+def create_tables():
+    init_db()
